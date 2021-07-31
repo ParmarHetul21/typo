ce729d6d002d634c0a3990c599b490bb96890a7a
Hetul Parmar <hetul.d.parmar.20@gmail.com>
2021-07-31 23:37:36 +0530

    compeleted

--- js/type.js
+++ js/type.js
@@ -1,7 +1,13 @@
 let quoteinput = document.getElementById("quoteInput");   
-const paragraph = 'I have been impressed with the urgency of doing Knowing is not enough we must apply Being willing is not enough we must do';
+const pharagraphs = [
+    'I have been impressed with the urgency of doing Knowing is not enough we must apply Being willing is not enough we must do The oldest classical Greek and Latin writing had little or no space between words and could',
+    'be written in boustrophedon The Rocker is a 2008 American comedy film directed by Peter Cattaneo and written by Maya Forbes and Wallace Wolodarsky, from a story by Ryan Jaffe. The film stars Rainn Wilson as a failed',
+    'musician who goes on tour with his nephews band after one of their songs goes viral.'
+];
 
-let text = paragraph.split('').map( (character) => {
+const textarea = pharagraphs[parseInt(Math.random() * pharagraphs.length)];
+
+let text = textarea.split('').map( (character) => {
     let span = document.createElement('span');
     span.innerText = character.toLowerCase();
     quoteinput.appendChild(span);
@@ -13,26 +19,23 @@ let firstCharacter = text[cursorIndex];
 firstCharacter.classList.add('correct');
 
 document.addEventListener('keydown', ({ key }) => {
-    if( key === firstCharacter.innerText ){
-        firstCharacter.classList.remove('correct');
-        firstCharacter.classList.add('cursor')
-        firstCharacter = text[++cursorIndex];
-        firstCharacter.classList.add('correct');
-    } else if( key !== firstCharacter.innerText && key === "Backspace" ){
-        firstCharacter.classList.remove('correct');
-        firstCharacter.classList.remove('cursor')
+    
+    if( key === "Backspace" ) {
+        firstCharacter.classList.add('incorrect');
+        firstCharacter.classList.remove('cursor');
         firstCharacter = text[--cursorIndex];
-        firstCharacter.classList.add('correct');
-    } else if( key === firstCharacter.innerText ){
-        
+    } else {
+        if( key === firstCharacter.innerText ) {
+            firstCharacter.classList.remove('incorrect');
+            firstCharacter.classList.remove('correct');
+            firstCharacter.classList.add('cursor')
+            firstCharacter = text[++cursorIndex];
+            firstCharacter.classList.add('correct');
+        } else {
+            firstCharacter.classList.add('incorrect');
+            firstCharacter.classList.remove('cursor');
+            firstCharacter = text[++cursorIndex];
+        }
     }
+     
 });
-
-
-
-
-
-
-
-
-
