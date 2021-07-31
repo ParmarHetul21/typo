0f5f48de3d0e5bfe56c9224ffc2f7cb500a1de6d
Hetul Parmar <hetul.d.parmar.20@gmail.com>
2021-07-31 18:05:21 +0530

    design changed

--- js/type.js
+++ js/type.js
@@ -1,48 +1,38 @@
-const timer      = document.querySelector("#timer");
-let quotedisplay = document.getElementById("quoteDisplay");
-let quoteinput   = document.getElementById("quoteInput");
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+let quoteinput = document.getElementById("quoteInput");   
+const paragraph = 'I have been impressed with the urgency of doing Knowing is not enough we must apply Being willing is not enough we must do';
 
-quoteinput.addEventListener( 'input', () => {
+let text = paragraph.split('').map( (character) => {
+    let span = document.createElement('span');
+    span.innerText = character.toLowerCase();
+    quoteinput.appendChild(span);
+    return span; 
+});
 
-    const arrayText = quotedisplay.querySelectorAll("span");
-    const arrayValue = quoteinput.value.split('');
-    let correct = true
-    arrayText.forEach( (characterspan, index) => {
-        const character = arrayValue[index];
-        if (character == null) {
-            characterspan.classList.remove('correct');
-            characterspan.classList.remove('incorrect');
-            correct = false;
-        } else if( character === characterspan.innerText){
-            characterspan.classList.add('correct');
-            characterspan.classList.remove('incorrect');
-        } else {
-            characterspan.classList.remove('correct');
-            characterspan.classList.add('incorrect');
-            correct = false;
-        }
-    });
-    
-    if ( correct ) renderData();
+let cursorIndex = 0;
+let firstCharacter = text[cursorIndex];
+firstCharacter.classList.add('correct');
+
+document.addEventListener('keydown', ({ key }) => {
+    if( key === firstCharacter.innerText ){
+        firstCharacter.classList.remove('correct');
+        firstCharacter.classList.add('cursor')
+        firstCharacter = text[++cursorIndex];
+        firstCharacter.classList.add('correct');
+    } else if( key !== firstCharacter.innerText && key === "Backspace" ){
+        firstCharacter.classList.remove('correct');
+        firstCharacter.classList.remove('cursor')
+        firstCharacter = text[--cursorIndex];
+        firstCharacter.classList.add('correct');
+    } else if( key === firstCharacter.innerText ){
+        
+    }
 });
 
 
-function getRandomText() {
-    return fetch(RANDOM_QUOTE_API_URL)
-    .then(response => response.json())
-    .then(data => data.content)
-}
 
-async function renderData() {
-    let splittedData = await getRandomText();
-    quotedisplay.innerHTML = '';
-    quoteinput.innerHTML = null;
-    splittedData.split('').forEach( character => {
-        let spanElement = document.createElement('span'); 
-        spanElement.innerHTML = character;
-        quotedisplay.appendChild(spanElement);
-    });
-    quoteinput.value = null;
-}
-renderData();
+
+
+
+
+
+
