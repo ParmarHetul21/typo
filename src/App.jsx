da4d15ba570ae4ee2b01cf3970b8861b2561d341
ParmarHetul21 <hetul.d.parmar.20@gmail.com>
2022-02-17 11:08:36 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Typo</h1>
+				<h4>Under construction</h4>
+			</header>
+		</div>
+	);
+}
+
+export default App;
